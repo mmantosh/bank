fd8c3b586ce9edb8eac90d9fd4a943f3637bb54c
mmantosh <mantosh.mohanty@yahoo.com>
2017-03-27 11:53:17 +0530

    commit after adding commment

--- SpringHibernateExample/src/main/java/com/websystique/springmvc/controller/AppController.java
+++ SpringHibernateExample/src/main/java/com/websystique/springmvc/controller/AppController.java
@@ -24,6 +24,7 @@ import com.websystique.springmvc.service.EmployeeService;
 //message "helloWorld"
 //9853558675
 //Rakesh sahoo
+//RIP
 @Controller
 @RequestMapping("/")
 public class AppController {
